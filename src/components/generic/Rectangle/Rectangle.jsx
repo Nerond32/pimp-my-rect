a1d8223c551e6b1f9ad9d9abeb7657900c7b1b82
Nerond32 <nerond32@gmail.com>
2019-08-24 21:36:19 +0200

    Rectangle overhaul, scalable dynamically within container

--- src/components/generic/Rectangle/Rectangle.jsx
+++ src/components/generic/Rectangle/Rectangle.jsx
@@ -1,14 +1,32 @@
-import React, { memo } from 'react';
+import React, { memo, useLayoutEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 const Rectangle = ({ borderSize, color, height, width }) => {
+  const realHeight = height + 2 * borderSize;
+  const realWidth = width + 2 * borderSize;
+  const rectRef = useRef();
+  const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
+  useLayoutEffect(() => {
+    if (rectRef.current) {
+      setDimensions({
+        width: rectRef.current.parentElement.offsetWidth,
+        height: rectRef.current.parentElement.offsetHeight
+      });
+    }
+  }, []);
+  const heightProportion = dimensions.height / realHeight;
+  const widthProportion = dimensions.width / realWidth;
+  let multiplier = 1;
+  if (heightProportion < 1 || widthProportion < 1) {
+    multiplier = Math.min(heightProportion, widthProportion);
+  }
   const rectStyle = {
-    border: `solid black ${borderSize}px`,
     backgroundColor: `${color}`,
-    height: `${height}px`,
-    width: `${width}px`
+    border: `${Math.ceil(borderSize * multiplier)}px solid black`,
+    height: `${Math.ceil(height * multiplier)}px`,
+    width: `${Math.ceil(width * multiplier)}px`
   };
-  return <div className="rectangle" style={rectStyle} />;
+  return <div ref={rectRef} className="rectangle" style={rectStyle} />;
 };
 
 Rectangle.propTypes = {
